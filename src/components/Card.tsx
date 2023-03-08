f2380d33b43205374a2e1acead9ffeb0376f4816
Andrew Owino <andystudy4@gmail.com>
2023-03-08 22:59:37 +0300

    add more details to the cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -19,6 +19,13 @@ function Card({ launch }: CardProps): JSX.Element {
   return (
     <div style={{ border: '1px solid black' }}>
       <p>{launch.name}</p>
+      <p>{launch.date_utc}</p>
+      <p>
+        Status:
+        {' '}
+        {launch.success ? 'Success' : 'Failed'}
+      </p>
+      <p>{launch.upcoming ? 'Upcoming' : false }</p>
     </div>
   );
 }
